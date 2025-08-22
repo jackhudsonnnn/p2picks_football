4f30b826d858269df873d788107711c6e09c3624
Jack Hudson <j4ckhudson111@gmail.com>
2025-08-21 23:56:53 -0400

    add&remove cleaner ux

--- client/src/features/social/index.ts
+++ client/src/features/social/index.ts
@@ -1,6 +1,5 @@
 export * from './types';
 export * from './service';
 export * from './hooks';
-export * from './ui/FriendsList/FriendsList';
 export * from './ui/ProfileCard/ProfileCard';
 export * from './ui/FriendsManager/FriendsManager';
--- client/src/pages/TablesListPage/TablesListPage.tsx
+++ client/src/pages/TablesListPage/TablesListPage.tsx
@@ -54,10 +54,6 @@ export const TablesListPage: React.FC = () => {
     <div className="tables-list-page">
       <PageHeader title="My Tables" actionButton={
         <button className="create-button" onClick={() => setIsCreateModalOpen(true)} disabled={loading}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <line x1="12" y1="5" x2="12" y2="19"></line>
-            <line x1="5" y1="12" x2="19" y2="12"></line>
-          </svg>
           Create Table
         </button>
       } />
